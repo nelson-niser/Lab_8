2c4cf253e9988597a0393c7891902ff1ce2d66cc
nelsonksh <42182216+nelsonksh@users.noreply.github.com>
2018-10-23 21:54:26 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,19 +1,39 @@
 #include<iostream>
 using namespace std;
 
-void f(int a1[],int a2[], int arr[])
+int largest(int arr[])
 {
-
-	for(int i=0; i<9; i++)
+	int a=arr[0];
+	for (int i=1; i<9; i++)
 	{
-	if(i<5)	arr[i]=a1[i];
-	else	arr[i]=a2[i-5];
-	
-	cout << arr[i] << "   ";
+	if (a>arr[i])	a = a;
+	else	a = arr[i];
 	}
+	return a;
+}
 
-//arranging in ascending order
-for (int i=0; i<9; i++)
+int smallest(int arr[])
+{
+	int a=arr[0];
+	for (int i=1; i<9; i++)
+	{
+	if (a<arr[i])	a = a;
+	else	a = arr[i];
+	}
+	return a;
+}
+
+double mean(int arr[])
+{
+	double a = 0;
+	for (int i=0; i<9; i++)
+	{	a = a + arr[i];	}
+	return a/9;
+}
+
+int median(int arr[])
+{
+	for (int i=0; i<9; i++)
 	{
 		for (int j=i; j<9; j++)
 		{
@@ -24,21 +44,53 @@ for (int i=0; i<9; i++)
 			arr[j] = a;
 		}	}
 	}
-cout << endl;
-cout << "The maximum number is: " << arr[0] << endl;
-cout << "The minimum number is: " << arr[8] << endl;
+return arr[4];
 }
 
+int highest_frequency(int arr[])
+{
+//sorting
+	for (int i=0; i<9; i++)
+	{
+		for (int j=i; j<9; j++)
+		{
+		if (arr[i] > arr[j])
+			{
+			int a = arr[i];
+			arr[i] = arr[j];
+			arr[j] = a;
+		}	}
+	}
+//find max frequency
+int maxF=1, countF=1, ElementF;
+	for (int i=0; i<8; i++)
+	{
+	if(arr[i]==arr[i+1])	countF++;
+	else
+		{
+		if(countF>maxF)
+			ElementF = arr[i-1];
+			maxF = countF;
+			countF = 1;
+		}
+	}
+
+return ElementF;
+}
 
 int main()
 {
+int arr[9] = {5,4,9,5,3,7,8,7,7};
 
-int a1[5] = {1,2,3,4,5};
-int a2[4] = {6,7,8,9};
-int arr[9];
 
-f(a1,a2,arr);
+cout << "My array = {5,4,9,5,3,7,8,7,7}" << endl;
 
+
+cout << "Largest element = " << largest(arr) << endl;
+cout << "Smallest element = " << smallest(arr) << endl;
+cout << "Mean = " << mean(arr) << endl;
+cout << "Median = " << median(arr) << endl;
+cout << "Element with highest frequency = " << highest_frequency(arr) << endl;
 
 return 0;
 }
